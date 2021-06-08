2aa74c23550fbff471fbc2fa57da83b4b237efe7
Leonardo Migdady <leonardo.mig@hotmail.com>
2021-06-08 16:28:46 -0300

    Implementing chess position

---
+++ Chess_Console/Chess_Console/Chess/ChessPosition.cs
@@ -0,0 +1,30 @@
+﻿using Chess_Console.Board;
+
+namespace Chess_Console.Chess
+{
+    class ChessPosition
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public ChessPosition()
+        {
+        }
+
+        public ChessPosition(int row, char column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
