e4b20457c6e1db58d78c0ba5329ca128255e7088
Leonardo Migdady <leonardo.mig@hotmail.com>
2021-06-08 14:43:09 -0300

    Position class created

---
+++ Chess_Console/Chess_Console/Board/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Board
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Row { get; set; }
+
+        public Position(int line, int row)
+        {
+            Line = line;
+            Row = row;
+        }
+        
+        public override string ToString()
+        {
+            return $"{Line}, {Row}";
+        }
+    }
+}
---
+++ Chess_Console/Chess_Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Board;
+
+namespace Chess_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
